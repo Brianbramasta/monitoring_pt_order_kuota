e563381ea344ff9e6711d323b12b05d362871605
Brianbramasta <Brianaldybramasta@gmail.com>
2025-07-01 11:24:07 +0700

    Memperbarui URL base pada apiClient.js untuk menggunakan variabel lingkungan dengan fallback ke URL default, meningkatkan fleksibilitas dalam pengujian dan pengembangan.

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   // baseURL: 'https://8cf924a4-47f2-4765-b937-a945e2dd0f8c-00-2xdk8t32t8xn0.pike.replit.dev', // Ganti ke URL API asli nanti
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Ganti ke URL API asli nanti
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://monitoring-pt-order-kuota.vercel.app', // Ganti ke URL API asli nanti
   // baseURL: 'https://monitoring-pt-order-kuota.vercel.app', // Ganti ke URL API asli nanti
   headers: {
     'Content-Type': 'application/json',
